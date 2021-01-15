231cbd2434ea9a580c58245b7e1f8733669cde85
kiritowu <70313929+kiritowu@users.noreply.github.com>
2021-01-16 00:31:58 +0800

    JWT Cookies Login

--- auth/tokenAuth.js
+++ auth/tokenAuth.js
@@ -17,14 +17,13 @@ var config = require('../config');
 module.exports = {
     verifyToken: (req, res, next) => {
         // console.log(req.headers);
-        var token = req.headers.authorization; //retrieve authorization header’s content
-        //console.log(token);
+        var token = req.cookies.authcookie; //retrieve authorization header’s content
+        console.log(token);
 
-        if (!token || !token.includes('Bearer')) { //process the token
+        if (!token) { //process the token
             res.status(403);
             return res.send({auth: 'false', message: 'Not authorized!'});
         } else {
-            token = token.split('Bearer ')[1]; //obtain the token’s value
             // console.log(token);
             jwt.verify(token, config.key, (err, decoded) => {//verify token
                 if (err) {
@@ -38,6 +37,18 @@ module.exports = {
                 }
             });
         }
+    },
+    checkToken:(req, res, token)=>{
+        jwt.verify(token, config.key, (err, decoded) => {//verify token
+            if (err) {
+                res.status(403);
+                return res.send({auth: 'false', message: 'Not authorized!'});
+            } else {
+                // console.log(decoded);
+                req.user_id = decoded.id; //decode the userid and store in req for use
+                req.type = decoded.type; //decode the type and store in req for use
+            }
+        });
     },
     generateToken: async (user_id, type, expires = 86400) => {
         // console.log(user_id)
